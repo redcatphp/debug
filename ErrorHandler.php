20bba9489e36e6ba5be440eab052b7768230bcdd
surikat <jo@surikat.pro>
2016-01-08 17:29:22 +0100

    add getExceptionTraceAsString to get full trace

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -62,13 +62,50 @@ class ErrorHandler{
 				echo ':';
 				var_dump($e->getData());
 			}
-			echo htmlentities($e->getTraceAsString());
+			//echo htmlentities($e->getTraceAsString());
+			echo htmlentities($this->getExceptionTraceAsString($e));
 			echo '</pre>';
 		}
 		else{
 			echo strip_tags($msg);
 		}
 		return false;
+	}
+	function getExceptionTraceAsString($exception) {
+		$rtn = "";
+		$count = 0;
+		foreach ($exception->getTrace() as $frame) {
+			$args = "";
+			if (isset($frame['args'])) {
+				$args = array();
+				foreach ($frame['args'] as $arg) {
+					if (is_string($arg)) {
+						$args[] = "'" . $arg . "'";
+					} elseif (is_array($arg)) {
+						$args[] = "Array";
+					} elseif (is_null($arg)) {
+						$args[] = 'NULL';
+					} elseif (is_bool($arg)) {
+						$args[] = ($arg) ? "true" : "false";
+					} elseif (is_object($arg)) {
+						$args[] = get_class($arg);
+					} elseif (is_resource($arg)) {
+						$args[] = get_resource_type($arg);
+					} else {
+						$args[] = $arg;
+					}   
+				}   
+				$args = join(", ", $args);
+			}
+			$rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
+									 $count,
+									 isset($frame['file']) ? $frame['file'] : 'unknown file',
+									 isset($frame['line']) ? $frame['line'] : 'unknown line',
+									 (isset($frame['class']))  ? $frame['class'].$frame['type'].$frame['function'] : $frame['function'],
+									 $args );
+			$count++;
+		}
+		return $rtn;
 	}
 	function errorHandle($code, $message, $file, $line){
 		if(!$this->handle||error_reporting()==0)
