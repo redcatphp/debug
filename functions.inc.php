e0414c18ab54534b7f94bc8674b48b8f63e2eba2
surikat <jo@surikat.pro>
2016-07-22 01:09:42 +0200

    dj an ddj function: debug via json serialize api

--- functions.inc.php
+++ functions.inc.php
@@ -30,3 +30,18 @@ if(!function_exists('dd')){
 		die;
 	}
 }
+if(!function_exists('dj')){
+	function dj(){
+		$args = func_get_args();
+		foreach($args as &$arg){
+			$arg = json_encode($arg,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+		}
+		return call_user_func_array(['RedCat\Debug\Vars',php_sapi_name()=='cli'?'debugsCLI':'debugs'],$args);
+	}
+}
+if(!function_exists('ddj')){
+	function ddj(){
+		call_user_func_array('dj',func_get_args());
+		die;
+	}
+}
